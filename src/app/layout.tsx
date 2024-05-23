e1d511f1e62b5b82e3c2c41f8c9939e511a2b6de
hzkonor <hzk@konor.fr>
2024-05-23 11:43:37 +0200

    v0

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Expérience de stage",
+  description: "Tentez l'expérience de l'évaluation des haies du Pilat !",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import { QuizForm } from "@/components/quiz-form"
+
+export default function Home() {
+  return (
+    <QuizForm />
+  );
+}
