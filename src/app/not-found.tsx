0ffc592c262c50ed9033b83f979069ee18ccf412
hzkonor <hzk@konor.fr>
2024-05-27 09:21:32 +0200

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 text-center">
             <h2 className="text-3xl font-bold mb-6">Erreur : page non trouvée</h2>
-            <p>La page demandée n'existe pas</p>
+            <p>La page demandée n&apos;existe pas</p>
             <Link href="/">
                 <Button
                     className="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors w-full sm:w-auto mt-10"
