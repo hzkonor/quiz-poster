6d0b6619c424732eb097e27653dcfed72eb1dd26
hzkonor <hzk@konor.fr>
2024-05-30 15:45:24 +0200

    add color

--- src/components/quiz-end.tsx
+++ src/components/quiz-end.tsx
@@ -11,13 +11,17 @@ export function QuizEnd({ score }: QuizEndProps) {
     return (
         <div className='flex flex-col items-center justify-center min-h-screen mx-4'>
             <h1 className="text-3xl font-bold my-6 text-center">Félicitations, vous êtes arrivé.es à la fin de cette expérience immersive !</h1>
-            {score <= 3 ? (
-                "Encore un peu de travail pour pouvoir faire ce stage, votre score est de " + score + " sur 10"
-            ) : score <= 7 ? (
-                "C\'est pas mal, vous êtes presque prêt.es pour faire ce stage ! Votre score est de " + score + " sur 10"
-            ) : (
-                "Wouah, vous êtes un.e expert.e, vous pouvez dès à présent vous lancer dans l'expérience du stage avec un score de " + score + " sur 10"
-            )}
+            <p dangerouslySetInnerHTML={{
+                __html:
+                    (score <= 3 ? (
+                        "Encore un peu de travail pour pouvoir faire ce stage, votre score est de "
+                    ) : score <= 7 ? (
+                        "C'est pas mal, vous êtes presque prêt.es pour faire ce stage ! Votre score est de "
+                    ) : (
+                        "Wouah, vous êtes un.e expert.e, vous pouvez dès à présent vous lancer dans l'expérience du stage avec un score de "
+                    )) + "<span class='font-bold text-amber-800 dark:text-amber-200'>" + score + " sur 10</span>"
+            }}></p>
+
             {/* 
             <div className="mt-10 bg-amber-200 dark:bg-amber-800 rounded-lg text-amber-800 dark:text-amber-200 p-4 flex items-center space-x-2">
                 <BadgeAlertIcon className="w-8 h-8" />
