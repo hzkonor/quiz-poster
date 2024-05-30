1db8ff28a6a2b02da4c30da7eabcf418bb502cc8
hzkonor <hzk@konor.fr>
2024-05-30 16:09:51 +0200

    chore: Update metadata for open graph tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Expérience de stage",
   description: "Tentez l'expérience de l'évaluation des haies du Pilat !",
+  openGraph: {
+    title: 'Expérience de stage',
+    description: "Tentez l'expérience de l'évaluation des haies du Pilat !",
+    url: 'https://poster-haies-cvb.vercel.app/',
+    siteName: 'Next.js',
+    images: [
+      {
+        url: 'https://poster-haies-cvb.vercel.app/background.jpg', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      }
+    ],
+    locale: 'fr_FR',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
