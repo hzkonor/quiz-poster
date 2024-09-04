eebadaaece146209e49e5acce85c270f2279c4c5
Conor <hzk@konor.fr>
2024-09-04 07:10:55 +0000

    update style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@components/ui/button";
 import Link from 'next/link';
 import { Analytics } from "@vercel/analytics/react"
 
@@ -17,7 +16,7 @@ export default function Home() {
           <h2 className="text-xl mb-6 text-center">Evaluation des haies du Contrat Vert et Bleu du Parc Naturel Régional du Pilat</h2>
           <div className="flex flex-col items-center space-y-4 mx-auto mt-8">
             <a href="/Poster_Sallier.pdf" target="_blank" rel="noopener noreferrer" download>
-              <button className="btn">Télécharger le poster</button>
+              <button className="btn btn-outline">Télécharger le poster</button>
             </a>
             <Link href="/quiz">
             <button className="btn btn-accent">Aller au quiz immersif</button>
--- src/components/quiz-form.tsx
+++ src/components/quiz-form.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { Button } from "@/components/ui/button";
 import React, { useState } from 'react';
 import Image from "next/image";
 
